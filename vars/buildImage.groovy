65c2c52477e8ab0e521d762615dda6a4f83ee977
Mahta Zare <mahtazare77@gmail.com>
2025-11-18 15:45:11 -0600

    initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "building image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'docker build -t mahtazare/test_repo:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push mahtazare/test_repo:jma-2.0'
+    }
+}

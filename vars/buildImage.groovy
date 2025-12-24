7f52e757d27fc1784519eede6bc11fd34e3d2194
mahta-zare <73464595+mahta-zare@users.noreply.github.com>
2025-12-24 16:11:35 -0600

    Restore Docker class usage in buildImage function
    Refactored buildImage.groovy to restore Docker class usage.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,19 +1,8 @@
 #!/usr/bin/env groovy
 
-// import com.example.Docker
-
-// def call(String imageName) {
-//     // this refers to the variables and methods from this groovy file that are going to be passed to the script of the Docker class
-//     return new Docker(this).buildDockerImage(imageName)
-// }
+import com.example.Docker
 
 def call(String imageName) {
-    echo 'building the image'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $imageName ."
-        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
-        sh "docker push $imageName"
-    }
-
+    // this refers to the variables and methods from this groovy file that are going to be passed to the script of the Docker class
+    return new Docker(this).buildDockerImage(imageName)
 }
-

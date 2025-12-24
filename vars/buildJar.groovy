403070fda1eed7f25f2b8c626b8f18a9049d97e5
mahta-zare <73464595+mahta-zare@users.noreply.github.com>
2025-12-24 15:07:49 -0600

    Update echo message in buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,13 @@
+// #!/usr/bin/env groovy
+
+// def call() {
+//     echo "building application for $BRANCH_NAME"
+//     sh 'mvn package'
+// }
+
 #!/usr/bin/env groovy
 
 def call() {
-    echo "building application for $BRANCH_NAME"
+    echo 'building the application'
     sh 'mvn package'
 }

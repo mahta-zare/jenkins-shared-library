ca6f9703161fefaf9d77fba66727dd61873a1d76
mahta-zare <73464595+mahta-zare@users.noreply.github.com>
2025-12-24 15:07:34 -0600

    Refactor buildImage.groovy to build and push Docker image
    Updated the buildImage.groovy script to build and push a Docker image with credentials.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,20 @@
+// #!/usr/bin/env groovy
+
+// import com.example.Docker
+
+// def call(String imageName) {
+//     // this refers to the variables and methods from this groovy file that are going to be passed to the script of the Docker class
+//     return new Docker(this).buildDockerImage(imageName)
+// }
+
 #!/usr/bin/env groovy
 
-import com.example.Docker
+def call() {
+    echo 'building the image'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t mahtazare/my-repo:jma-2.0 .'
+        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+        sh 'docker push mahtazare/my-repo:jma-2.0'
+    }
 
-def call(String imageName) {
-    // this refers to the variables and methods from this groovy file that are going to be passed to the script of the Docker class
-    return new Docker(this).buildDockerImage(imageName)
 }

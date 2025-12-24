1027ebb9ce3f3fc74686b7aedc02168ec1f63f36
mahta-zare <73464595+mahta-zare@users.noreply.github.com>
2025-12-24 15:13:23 -0600

    Fix shebang and clean up buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,11 +1,10 @@
-// #!/usr/bin/env groovy
+#!/usr/bin/env groovy
 
 // def call() {
 //     echo "building application for $BRANCH_NAME"
 //     sh 'mvn package'
 // }
 
-#!/usr/bin/env groovy
 
 def call() {
     echo 'building the application'

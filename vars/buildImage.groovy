75feb6252299067173eae44e7222b0d22757c91f
mahta-zare <73464595+mahta-zare@users.noreply.github.com>
2025-12-24 15:13:34 -0600

    Fix shebang line in buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,4 @@
-// #!/usr/bin/env groovy
+#!/usr/bin/env groovy
 
 // import com.example.Docker
 
@@ -6,8 +6,6 @@
 //     // this refers to the variables and methods from this groovy file that are going to be passed to the script of the Docker class
 //     return new Docker(this).buildDockerImage(imageName)
 // }
-
-#!/usr/bin/env groovy
 
 def call() {
     echo 'building the image'
